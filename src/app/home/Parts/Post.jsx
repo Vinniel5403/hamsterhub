ebba5ccabe5d8d4eb04ee41b81ffa958fb63eecf
Vinniel5403 <vinniel.gawintep@gmail.com>
2024-11-17 16:17:16 +0700

    final

--- src/app/home/Parts/Post.jsx
+++ src/app/home/Parts/Post.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Post.css'
 
+
 function Post() {
   return (
     <div className='post'>
-        <img  src="https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/451313802_526973819889868_735352169976925613_n.png?stp=dst-jpg&_nc_cat=110&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFpVwEDsLrJcSsGKnXN4DLRaujM1S18H2tq6MzVLXwfa_86GJSguH2suef5Pl3GSFsWK8GKWo4OSZHblTFFfrvV&_nc_ohc=endTyWLE-8sQ7kNvgFnb1P4&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=AHIE2ZTpIio4t0SqFEfvtjd&oh=00_AYChKe6lDO3pCi0B3nvM4R8DeAgJuUANfHEFEtvLMws4lg&oe=673E73DE" alt="" />
+        <img src='https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/466156450_592870276633555_6468443100540215578_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF9SbBCFta1Q0fCOeMb2DmnsUvSn2gs_f2xS9KfaCz9_aTuF0goOepH22jVPHaK0OgAgHyc9HxMjpaZjlFLQ0eJ&_nc_ohc=zmc-7p6XTvwQ7kNvgGHnL6n&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=AGgDEWbdT7JHJYC1Y2Bsqus&oh=00_AYDjCZ-6-Nkr0NnZNKEkPJRLYyrStVkUZUtq0tvKznDLJw&oe=673F87B7' alt="1" />
         <img src="https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/447740132_489922340261683_7475971680498206096_n.png?stp=dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeEkuExAwotHo83NGhCHTFQ3Ur8i-C03h4tSvyL4LTeHi3jcRl8__xmQCM52_jx9bchekRRGtSrSa-3qZ5Ev7g_b&_nc_ohc=uPTooiRFfEUQ7kNvgG8mOab&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=AwvO8426HeBPlucNN4AtRV_&oh=00_AYA78oNkqlQHQFiqudRL9GnGgAoic3j7sFfrGqgSqGx1Ng&oe=673E634C" alt="" />
         <img src="https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/308404817_1108137199804990_736898706354417086_n.png?stp=dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeE8J8P1KqgJn5w4n2Tm3FEm0wvqbLLY7CXTC-psstjsJdk1uyirLmNeYvvFnAjyLiTTGER8pdbTe8Bnd67O1cJ3&_nc_ohc=uzN33eng3CYQ7kNvgEfYosC&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=AQQRgjokcKyFcl26ql2WAVU&oh=00_AYC3tu4ggTJV8SjHuyQvuRbMictGeKShNtuuPzqV-zds9A&oe=673E7159" alt="" />
-        <img  src="https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/298548723_1081367869148590_1901934845732173601_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEH1Cde8JUeSQWWWp3E6wXhuGjUm80E39C4aNSbzQTf0H5Oq9Nlbk6z-hDL4v5hoEhX6N1aTT66ds4-lLqw-WJU&_nc_ohc=fiLNqzfXz7AQ7kNvgHNh6rH&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=AfpIWg1wz3B9wRycU5BhBNW&oh=00_AYDj8PNiZtMozjOAFXgNsFtowsZHNadXK16a5ZNv13VjgA&oe=673E46A3" alt="" />
-
+        <img src="https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/461937017_563719226215327_3907193350425357966_n.png?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeGvHqMhO_w4pOzk-3Cn5jyVssFus5-M_sKywW6zn4z-wvIuCBC9QngXvSfowbqlqwKgT9zo_6o4B7JhQRA6C2Nw&_nc_ohc=77RYAWJ9MPYQ7kNvgHYML0T&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=AqF6VZWQz9dYZ0HVEcVXKOW&oh=00_AYBxNm87r5U0EueINwOArwzVMbAupcxUljpq4DEqLR2C7A&oe=673F7BD9" alt="" />
     </div>
   )
 }
--- src/app/home/Parts/Review.jsx
+++ src/app/home/Parts/Review.jsx
@@ -10,27 +10,37 @@ function Review() {
     {
       title: "Lorem Test Page 1",
       description: "Lorem ipsum dolor sit amet consectetur.",
-      image: "https://via.placeholder.com/800x600?text=Slide+1",
+      image: "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/446668699_483959664191284_6159870530134804904_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFDNR7hUJdAb1DfYgTRH_LOjEjbXFLvrnmMSNtcUu-ueaJuqZuWwZSGPsED-IZXtGZEI9CwYQqqyEWuJ03TElhH&_nc_ohc=FpxljzVWWvUQ7kNvgEu_Gjg&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=AaW9p-QBJMa7B6ex1Ph0tE3&oh=00_AYCJKmVXsz8DZYiBdEyzsGiIhnitY7u9ZtacCxCP110sGw&oe=673F9A4D",
     },
     {
       title: "Lorem Test Page 2",
       description: "Lorem ipsum dolor sit amet consectetur.",
-      image: "https://via.placeholder.com/800x600?text=Slide+2",
+      image: "https://scontent.fbkk17-1.fna.fbcdn.net/v/t1.6435-9/66780508_399110890707628_360779009505624064_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFO5cjP4E4pFsnEliCb2DdN7DYeufVqCU7sNh659WoJTkul-4R53aDdar7RdG-NUt2M1A4CfGI4Fa9Wsw1apchf&_nc_ohc=MWfhK4c8odoQ7kNvgHJNyol&_nc_zt=23&_nc_ht=scontent.fbkk17-1.fna&_nc_gid=A6S4Qff9kvYKpRz2hdL2Rka&oh=00_AYDX6OVeSxzg71CllywWv9HOlH7fYX6U_bC0LrGpvcf1-w&oe=67612104",
     },
     {
       title: "Lorem Test Page 3",
       description: "Lorem ipsum dolor sit amet consectetur.",
-      image: "https://via.placeholder.com/800x600?text=Slide+3",
+      image: "https://img.freepik.com/free-photo/portrait-happy-asian-woman-smiling-posing-confident-cross-arms-chest-standing-against-studio-background_1258-89269.jpg?t=st=1731833100~exp=1731836700~hmac=2941f131de270af4e67b385e254a7f079aea20ee35bbe0e8bf32e73f974737d0&w=1380",
     },
     {
       title: "Lorem Test Page 4",
       description: "Lorem ipsum dolor sit amet consectetur.",
-      image: "https://via.placeholder.com/800x600?text=Slide+4",
+      image: "https://media.npr.org/assets/img/2021/11/16/gettyimages-1235223332_sq-e88ad790d447bd7dfcb0c1571047db26d39a8ee0.jpg?s=1100&c=85&f=jpeg",
     },
     {
       title: "Lorem Test Page 5",
       description: "Lorem ipsum dolor sit amet consectetur.",
-      image: "https://via.placeholder.com/800x600?text=Slide+5",
+      image:"https://img.freepik.com/free-photo/close-up-portrait-young-successful-african-american-adult-man-red-hoodie_176420-33869.jpg?t=st=1731834903~exp=1731838503~hmac=328b69226c8635dd51a2829debe3a58c702ae575a64c2b4ed680af8ea3cf59f3&w=1380",
+    },
+    {
+      title: "Lorem Test Page 5",
+      description: "Lorem ipsum dolor sit amet consectetur.",
+      image:"https://img.freepik.com/free-photo/medium-shot-man-posing-outside_23-2149028795.jpg?t=st=1731834837~exp=1731838437~hmac=5241877b2697609ff14137c21a1b0753f485ea29702d6e78a5c4cc88dda20289&w=1380",
+    },
+    {
+      title: "Lorem Test Page 5",
+      description: "Lorem ipsum dolor sit amet consectetur.",
+      image:"https://img.freepik.com/free-psd/portrait-young-man-with-afro-dreadlocks-hairstyle_23-2150164187.jpg?t=st=1731834874~exp=1731838474~hmac=4276d0925fe11fa81e33b043d915d834b5899d1496f356c8bd4c3c73c6d73b34&w=1380",
     },
   ];
 
@@ -71,6 +81,7 @@ function Review() {
         {reviews.map((review, index) => (
           <div key={index} className="slide">
             <img src={review.image} alt={`Slide ${index + 1}`} />
+            <p>{review.description}</p>
           </div>
         ))}
       </div>
