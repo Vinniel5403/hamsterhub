f39a1fbf4f0b82a969e19f350ef52f513a4e69d4
Vinniel5403 <vinniel.gawintep@gmail.com>
2024-11-25 23:47:19 +0700

    hotfix

--- src/app/home/Parts/Post.jsx
+++ src/app/home/Parts/Post.jsx
@@ -6,7 +6,7 @@ function Post() {
   // ข้อมูลที่ใช้ใน component
   const posts = [
     {
-      img: "https://media.discordapp.net/attachments/1288114105705828404/1309167412834467850/api.png?ex=674292ea&is=6741416a&hm=a7611c6592a68ba36b9b4c27e6b3cc2a4a445d4e17a54e1b8d1f55972aaa6def&=&format=webp&quality=lossless&width=1440&height=582",
+      img: "/assets/np.png",
       title: "National Project-2",
       subtitle: "2nd Winner NSC",
       about: "#",
