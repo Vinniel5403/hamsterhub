02d2ad2a4a7eb96fb60ae6e79ca6e61dd1d0ffde
Vinniel5403 <vinniel.gawintep@gmail.com>
2024-12-07 00:10:29 +0700

    tag fix

--- src/app/course/test/components/CourseTag.jsx
+++ src/app/course/test/components/CourseTag.jsx
@@ -5,19 +5,33 @@ import Link from "next/link";
 function CourseTag() {
   return (
     <div className="course-tag">
-      <div className="course-name">
-        
-        <h3>National Project</h3>
-        <p>เริ่ม 8 ม.ค. เวลา:19:00-21:00</p>
-        {/* <p>ระยะเวลา</p> */}
-        <h5>150 ชม.</h5>
+      <div className="course-tag-desktop">
+        <div className="course-price">
+          
+          <h3>National Project</h3>
+          <h3>฿19,990</h3>
+        </div>
         <hr />
+        <div className="course-links">
+        <p> 8 ม.ค. 68 - 22 ก.ย. 68 </p>
+          
+
+          <a href="#">Book</a>
+        </div>
       </div>
-      <div className="tag">
-      <h5>฿19,990</h5>
-    
+      <div className="course-tag-mobile">
+        <div className="course-name">
+          <h3>National Project</h3>
+          <p>เริ่ม 8 ม.ค. เวลา:19:00-21:00</p>
+          <p>ระยะเวลา</p>
+          <h4>150 ชม.</h4>
+          <hr />
+        </div>
+        <div className="tag">
+        <h3>฿19,990</h3>
+        <Link href="#">Book</Link>
+        </div>
       </div>
-      <Link href="#">Book</Link>
     </div>
   );
 }
