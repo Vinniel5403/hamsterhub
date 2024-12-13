bac7e70f834beb881d77664daa8eacb9400747c2
Vinniel5403 <vinniel.gawintep@gmail.com>
2024-12-13 21:32:00 +0700

    test light theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    
+    <html lang="en" data-theme="light">
+      
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
